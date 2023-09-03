8fc2d2f888d74954e1ac75d14afffda47944dadc
Beatris Ilieve <beatrisilieve@icloud.com>
2023-09-03 16:19:55 +0300

    Apps, Configurations
    - Created the project's apps,
    - Created each app an urls.py file,
    - Added urls paths,
    - Added the apps in the INSTALLED_APPS setting

--- catstagram/settings.py
+++ catstagram/settings.py
@@ -18,6 +18,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'catstagram.accounts',
+    'catstagram.cats',
+    'catstagram.common',
+    'catstagram.photos',
 ]
 
 MIDDLEWARE = [
--- catstagram/urls.py
+++ catstagram/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('catstagram.accounts.urls')),
+    path('cats/', include('catstagram.cats.urls')),
+    path('common/', include('catstagram.common.urls')),
+    path('photos/', include('catstagram.photos.urls')),
 ]
