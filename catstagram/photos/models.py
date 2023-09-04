98f32ef1e8029a1d9a9f23990870ceb0d9431ca7
Beatris Ilieve <beatrisilieve@icloud.com>
2023-09-04 14:18:48 +0300

    Created the Photo model
    - Added fields,
    - Registered model in the admin,
    - Installed pillow,
    - Made migrations,
    - Added the model in admin site

--- catstagram/photos/models.py
+++ catstagram/photos/models.py
@@ -1,3 +1,39 @@
+# photos/models.py
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from catstagram.cats.models import Cat
+
+
+class Photo(models.Model):
+    MIN_DESCRIPTION_LENGTH = 10
+    MAX_DESCRIPTION_LENGTH = 300
+
+    MAX_LOCATIONS_LENGTH = 30
+
+    photo = models.ImageField(
+        null=False,
+        blank=True,
+    )
+
+    description = models.CharField(
+        max_length=MAX_DESCRIPTION_LENGTH,
+        validators=(
+            MinLengthValidator(MIN_DESCRIPTION_LENGTH),
+        )
+    )
+
+    locations = models.CharField(
+        max_length=MAX_LOCATIONS_LENGTH,
+    )
+
+    publication_date = models.DateField(
+        auto_now=True,
+        null=False,
+        blank=False,
+    )
+
+    tagged_cats = models.ManyToManyField(
+        Cat,
+        blank=True,
+    )
