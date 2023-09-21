0d33694e284ef7cfefe6596bfaaa0624d655ee86
Beatris Ilieve <beatrisilieve@icloud.com>
2023-09-21 11:57:58 +0300

    Extending CatstagramUser model

--- catstagram/accounts/admin.py
+++ catstagram/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+CatstagramUserModel = get_user_model()
+
+
+@admin.register(CatstagramUserModel)
+class CatstagramUserAdmin(UserAdmin):
+    pass
--- catstagram/accounts/models.py
+++ catstagram/accounts/models.py
@@ -1,3 +1,56 @@
+from enum import Enum
+
+from django.contrib.auth.models import AbstractUser
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+
+class ChoicesEnumMixin:
+    @classmethod
+    def choices(cls):
+        return [(x.name, x.value) for x in cls]
+
+    @classmethod
+    def max_length(cls):
+        return max(len(name) for name, _ in cls.choices())
+
+
+class Gender(ChoicesEnumMixin, Enum):
+    female = 'Female'
+    male = 'Male'
+    do_not_show = 'Do not Show'
+
+
+class CatstagramUser(AbstractUser):
+    FIRST_NAME_MIN_LENGTH = 2
+    FIRST_NAME_MAX_LENGTH = 30
+
+    LAST_NAME_MIN_LENGTH = 2
+    LAST_NAME_MAX_LENGTH = 30
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LENGTH,
+        validators=(
+            validators.MinLengthValidator(
+                FIRST_NAME_MIN_LENGTH,
+            ),
+        ),
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LENGTH,
+        validators=(
+            validators.MinLengthValidator(
+                LAST_NAME_MIN_LENGTH,
+            ),
+        ),
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    gender = models.CharField(
+        choices=Gender.choices(),
+        max_length=Gender.max_length(),
+    )
